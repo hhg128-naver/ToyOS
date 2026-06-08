88bb9e76000eef53a82b7346fc1ef28429f4ffb7
hhg128-naver <hhg128@naver.com>
2026-06-09 08:11:52 +0900

    코드 정리

--- user/hello.c
+++ user/hello.c
@@ -1,17 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int arg) {
-    printf("Hello, Newlib World from ToyOS User Mode!\n");
-    printf("Received argument: %d\n", arg);
-    
-    void* ptr = malloc(1024);
-    if (ptr) {
-        printf("Malloc success! Address: %p\n", ptr);
-        free(ptr);
-    } else {
-        printf("Malloc failed!\n");
-    }
+int main(int arg)
+{
+	printf("Hello, Newlib World from ToyOS User Mode!\n");
+	printf("Received argument: %d\n", arg);
 
-    return 0;
+	void* ptr = malloc(1024);
+	if (ptr) 
+	{
+		printf("Malloc success! Address: %p\n", ptr);
+		free(ptr);
+	}
+	else 
+	{
+		printf("Malloc failed!\n");
+	}
+
+	return 0;
 }
